d26cb825468953d3bba3967a51fdb537ac082e62
Wakushi <mzozord@gmail.com>
2023-05-18 20:49:54 +0200

    - Added signup and login methods for auth/route

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require("express")
 const cors = require("cors")
 const mongoose = require("mongoose")
-const userRoutes = require("./routes/user")
 require("dotenv").config()
 
+// ROUTES
+const userRoutes = require("./routes/user")
+
+// ENVIRONMENT
 const MONGO_DATABASE_URL = process.env.MONGO_DATABASE_URL
 const AUTHORIZED_URL = process.env.AUTHORIZED_URL
 
@@ -17,19 +20,18 @@ mongoose
 	.then(() => console.log("Connexion à MongoDB réussie !"))
 	.catch(() => console.log("Connexion à MongoDB échouée !"))
 
+app.use(express.json())
 app.use(
 	cors({
 		origin: AUTHORIZED_URL
 	})
 )
 
-app.use(express.json())
-
 app.use("/api/auth", userRoutes)
 
-app.post("/api/books", (req, res) => {
-	console.log(req.body)
-	res.status(201).json({ message: "Object created !" })
-})
+// app.post("/api/books", (req, res) => {
+// 	console.log(req.body)
+// 	res.status(201).json({ message: "Object created !" })
+// })
 
 module.exports = app

ee2cdd7ea9a6845598290a36989b398a4c256728
Wakushi <mzozord@gmail.com>
2023-05-23 13:12:26 +0200

    Green code update - Added sharp to multer-config.js to convert imgs to webp - Updated routes accordingly

--- middleware/multer-config.js
+++ middleware/multer-config.js
@@ -1,22 +1,48 @@
-const multer = require("multer")
+const multer = require("multer");
+const sharp = require("sharp");
 
 const MIME_TYPES = {
-	"image/jpg": "jpg",
-	"image/jpeg": "jpg",
-	"image/png": "png"
-}
+  "image/jpg": "jpg",
+  "image/jpeg": "jpg",
+  "image/png": "png",
+};
 
-const storage = multer.diskStorage({
+const storage = multer.memoryStorage(); // Use memory storage instead of disk storage
 
-	destination: (req, file, callback) => {
-		callback(null, "images")
-	},
+const upload = multer({
+  storage,
+  fileFilter: (req, file, callback) => {
+    const extension = MIME_TYPES[file.mimetype];
+    const isSupportedType = Object.values(MIME_TYPES).includes(extension);
 
-	filename: (req, file, callback) => {
-		const name = file.originalname.split(" ").join("_")
-		const extension = MIME_TYPES[file.mimetype]
-		callback(null, name + Date.now() + "." + extension)
-	}
-})
+    if (isSupportedType) {
+      callback(null, true);
+    } else {
+      callback(new Error("Invalid file type."));
+    }
+  },
+});
 
-module.exports = multer({ storage }).single("image")
+const uploadSingleImage = upload.single("image");
+
+const convertToWebP = (req, res, next) => {
+  if (req.file) {
+    const name = req.file.originalname.split(" ").join("_");
+    const webpFileName = name + Date.now() + "." + "webp";
+
+    sharp(req.file.buffer)
+      .webp()
+      .toFile("images/" + webpFileName, (error) => {
+        if (error) {
+          next(error);
+        } else {
+          req.file.filename = webpFileName;
+          next();
+        }
+      });
+  } else {
+    next();
+  }
+};
+
+module.exports = { uploadSingleImage, convertToWebP };
--- routes/books.js
+++ routes/books.js
@@ -1,17 +1,30 @@
-const express = require("express")
-const booksController = require("../controllers/books")
-const auth = require("../middleware/auth")
-const multer = require("../middleware/multer-config")
-const isBookOwner = require("../middleware/isBookOwner")
-const router = express.Router()
+const express = require("express");
+const booksController = require("../controllers/books");
+const auth = require("../middleware/auth");
+const multer = require("../middleware/multer-config");
+const isBookOwner = require("../middleware/isBookOwner");
+const router = express.Router();
 
-router.get("/", booksController.getAllBooks)
-router.get("/bestrating", booksController.getBestBooks)
-router.get("/:id", booksController.getOneBook)
+router.get("/", booksController.getAllBooks);
+router.get("/bestrating", booksController.getBestBooks);
+router.get("/:id", booksController.getOneBook);
 
-router.post("/", auth, multer, booksController.createBook)
-router.put("/:id", auth, isBookOwner, multer, booksController.modifyBook)
-router.delete("/:id", auth, isBookOwner, booksController.deleteBook)
-router.post("/:id/rating", auth, booksController.rateBook)
+router.post(
+  "/",
+  auth,
+  multer.uploadSingleImage,
+  multer.convertToWebP,
+  booksController.createBook
+);
+router.put(
+  "/:id",
+  auth,
+  isBookOwner,
+  multer.uploadSingleImage,
+  multer.convertToWebP,
+  booksController.modifyBook
+);
+router.delete("/:id", auth, isBookOwner, booksController.deleteBook);
+router.post("/:id/rating", auth, booksController.rateBook);
 
-module.exports = router
+module.exports = router;
